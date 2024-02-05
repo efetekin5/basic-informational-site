34dea3a7fbe3795c4f79571811e050621f288dfe
efe <121581223+efetekin5@users.noreply.github.com>
2024-02-05 17:24:21 +0300

    redo the project using express

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-const http = require('node:http')
-const fs = require('node:fs')
+const express = require('express');
+const path = require('path');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    let filePath = '.' + req.url + '.html';
-    if(req.url === '/') {
-        filePath = './index.html';
-    }
-
-    fs.readFile(filePath, (err, content) => {
-        if(err) {
-            if(err.code === 'ENOENT') {
-                fs.readFile('./404.html', (error, errorContent) => {
-                    res.writeHead(404, {'Content-Type': 'text/html'});
-                    res.end(errorContent, 'utf-8');
-                })
-            } else {
-                res.writeHead(500, {'Content-Type': 'text/html'});
-                res.end('Interval Server Error');
-            }
-        } else {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(content, 'utf-8')
-        }
-    })
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 })
 
-server.listen(8080);
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'about.html'));
+})
+
+app.get('/contact-me', (req, res) => {
+    res.sendFile(path.join(__dirname, 'contact-me.html'));
+})
+
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '404.html'))
+})
+
+app.listen(8080, () => {
+    console.log('app listening port 8080');
+})
